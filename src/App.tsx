df9c8c6051bef975a0a636159e82f0443be52678
jestersunborn <jestersunborn@gmail.com>
2024-11-09 23:49:37 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from 'preact/hooks';
 import styled from 'styled-components';
-import { invoke } from '@tauri-apps/api/core';
 import './App.css';
 
 const Title = styled.h1`
   color: white;
 `;
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState('');
-  const [name, setName] = useState('');
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke('greet', { name }));
-  }
-
+const App = () => {
   return (
     <main class="container">
       <Title>Welcome to Mjolnir</Title>
@@ -23,22 +13,6 @@ function App() {
       <div class="row">
         <img src="/logo.svg" class="logo" alt="Mjolnir logo" />
       </div>
-
-      <form
-        class="row"
-        onSubmit={e => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onInput={e => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
